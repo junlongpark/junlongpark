8c72fff0d6150a594fe4308feaba25687cab677b
junlongpark <167068127+junlongpark@users.noreply.github.com>
2025-05-16 17:08:14 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 788 ms, Memory: 130936 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,12 +1,13 @@
+import sys
+input = sys.stdin.readline
 from collections import Counter
+n=int(input())
 
-n = int(input())
 cards = list(map(int, input().split()))
-card_count = Counter(cards)  # 카드별 개수를 딕셔너리 형태로 저장
+cards_count = Counter(cards)
 
-m = int(input())
-targets = list(map(int, input().split()))
+m=int(input())
+target = list(map(int, input().split()))
 
-for num in targets:
-    print(card_count[num], end=' ')
-
+for num in target:
+    print(cards_count[num], end=' ')
